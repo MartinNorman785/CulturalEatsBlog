6e41503247965481e70c33a57150a7b5804e5cc5
MartinNorman785 <mnorman26@student.sacs.nsw.edu.au>
2025-07-29 19:05:47 +1000

    add image

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -12,7 +12,7 @@ export const restaurants: RestaurantType[] = [
     rating: 4.3,
     priceRange: "$$",
     imageUrl:
-      "https://images.unsplash.com/photo-1559329007-40df8a9345d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      "./public/casamami.jpg",
     contactInfo: {
       phone: "0450 626 477",
       email: "info@mamicasa.com",
