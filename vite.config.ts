773dce56f8b4e090ac29ca9a9638b48e8b97a92e
MartinNorman785 <mnorman26@student.sacs.nsw.edu.au>
2025-08-12 15:11:13 +1000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
